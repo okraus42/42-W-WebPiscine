0e26258b3aae6d380429404c11ac0b39105efb5d
okraus <okraus42+github@pm.me>
2024-05-17 10:13:13 +0200

    working on rush

--- cell03/ex04/ex00bis/backgroound.js
+++ cell03/ex04/ex00bis/backgroound.js
@@ -1,19 +1,18 @@
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function backgroundRandomiser() {
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+	let string = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")";
+	return string;
 }
 
 function changeBackgroundColor() {
-  const newColor = getRandomColor();
-  $('body').css('background-color', newColor);
+	const newColor = backgroundRandomiser();
+	$('body').css('background-color', newColor);
 }
 
 $(document).ready(function() {
-  $('#changeColorButton').click(changeBackgroundColor);
+	$('#changeColorButton').click(changeBackgroundColor);
 });
 
   
--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,53 +1,54 @@
-$(document).ready(function() {
-  // Function to create a new TO-DO
-  function createToDo() {
-    const todoText = prompt("Enter a new TO-DO:");
-    if (todoText) {
-      const todoDiv = $("<div>").text(todoText);
-      todoDiv.click(removeToDo);
-      $("#ft_list").prepend(todoDiv);
-      saveToCookie();
-    }
-  }
 
-  // Function to remove a TO-DO
-  function removeToDo() {
-    const shouldRemove = confirm("Do you want to remove this TO-DO?");
-    if (shouldRemove) {
-      $(this).remove();
-      saveToCookie();
-    }
-  }
+window.onload = function() {
+function newToDo() {
+		const todoText = prompt("Enter a new TO-DO:");
+		if (todoText) {
+			const todoDiv = document.createElement("div");
+			todoDiv.textContent = todoText;
+			todoDiv.addEventListener("click", delToDo);
+			document.getElementById("ft_list").prepend(todoDiv);
+			saveToCookie();
+		}
+	}
+	
+	function delToDo() {
+		const shouldRemove = confirm("Do you want to remove this TO-DO?");
+		if (shouldRemove) {
+			this.remove();
+			saveToCookie();
+		}
+	}
+	
+	function saveToCookie() {
+		const todos = [];
+		const todoElements = document.querySelectorAll("#ft_list > div");
+		for (const todo of todoElements) {
+			todos.push(todo.textContent);
+		}
+		console.log(todos);
+		const todoListString = JSON.stringify(todos);
+		console.log(todoListString );
+		document.cookie = `todoList=${todoListString}; max-age=31536000; SameSite=None; Secure; path=/`;
+	}
+	
+	function loadFromCookie() {
+		//removing the todolist from the cookie
+		const cookieValue = document.cookie.substring(9);
 
-  // Function to save the TO-DOs as cookies
-  function saveToCookie() {
-    const todos = [];
-    $("#ft_list > div").each(function() {
-      todos.push($(this).text());
-    });
-    const todoListString = JSON.stringify(todos);
-    document.cookie = `todoList=${todoListString}`;
-  }
-
-  // Function to load TO-DOs from cookies
-  function loadFromCookie() {
-    const cookieValue = document.cookie.replace(
-      /(?:(?:^|.*;\s*)todoList\s*=\s*([^;]*).*$)|^.*$/,
-      "$1"
-    );
-    if (cookieValue) {
-      const todos = JSON.parse(cookieValue);
-      for (const todoText of todos) {
-        const todoDiv = $("<div>").text(todoText);
-        todoDiv.click(removeToDo);
-        $("#ft_list").append(todoDiv);
-      }
-    }
-  }
-
-  // Attach event listeners
-  $("#newButton").click(createToDo);
-
-  // Load TO-DOs from cookies when the page loads
-  loadFromCookie();
-});
+		console.log(document.cookie);
+		console.log(cookieValue);
+		if (cookieValue) {
+			const todos = JSON.parse(cookieValue);
+			for (const todoText of todos) {
+				const todoDiv = document.createElement("div");
+				todoDiv.textContent = todoText;
+				todoDiv.addEventListener("click", delToDo);
+				document.getElementById("ft_list").append(todoDiv);
+			}
+		}
+	}
+	
+	document.getElementById("newButton").addEventListener("click", newToDo);
+	
+	loadFromCookie();
+}
